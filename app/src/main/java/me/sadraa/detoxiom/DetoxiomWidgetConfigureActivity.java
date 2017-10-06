551f2767c22ed509a2e558d6fa1a9a9d97b53e5c
01sadra <01sadra@gmail.com>
2017-10-06 14:05:34 +0330

    Set Actionbar for widget conf activity

--- app/src/main/java/me/sadraa/detoxiom/DetoxiomWidgetConfigureActivity.java
+++ app/src/main/java/me/sadraa/detoxiom/DetoxiomWidgetConfigureActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,7 +16,7 @@ import java.util.List;
 /**
  * The configuration screen for the {@link DetoxiomWidget DetoxioWidget} AppWidget.
  */
-public class DetoxiomWidgetConfigureActivity extends Activity {
+public class DetoxiomWidgetConfigureActivity extends AppCompatActivity {
     PackageManager pm;
     List<ApplicationInfo> listOfAppInfo;
     ArrayList<String> nameOfAppsArray = new ArrayList<String>();
@@ -29,6 +31,16 @@ public class DetoxiomWidgetConfigureActivity extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.detoxiom_widget_configure);
+        //set Toolbar
+        Toolbar confToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(confToolbar);
+        //setting logo and title for app dynamicly
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setTitle("Pick app you want");
+        getSupportActionBar().setSubtitle("hello");
+
         createArrayFromApps();
         listView = (ListView) findViewById(R.id.list_view_conf_activity);
         listView.setAdapter(new ListViewCostumAdapter(this,nameOfAppsArray,appLogosArray));
@@ -43,7 +55,7 @@ public class DetoxiomWidgetConfigureActivity extends Activity {
         //Create two arrayList from name and logos of installed apps
 
         pm = getPackageManager();
-        listOfAppInfo = pm.getInstalledApplications(pm.GET_GIDS);
+        listOfAppInfo = pm.getInstalledApplications(pm.GET_META_DATA);
 
         for(ApplicationInfo app : listOfAppInfo) {
 
