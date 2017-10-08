8668f1e7c91b2e1570b3797e613db7415383cd00
01sadra <01sadra@gmail.com>
2017-10-08 17:31:35 +0330

    Fragments added

--- app/src/main/java/me/sadraa/detoxiom/MainActivity.java
+++ app/src/main/java/me/sadraa/detoxiom/MainActivity.java
@@ -1,18 +1,24 @@
 package me.sadraa.detoxiom;
 
 
+import android.support.annotation.IdRes;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
 
 public class MainActivity extends AppCompatActivity {
+    FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set toolbar
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -20,9 +26,39 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setTitle("Detoxiom");
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.contentContainer,new ArchiveFragment());
-        ft.commit();
+
+        //define listener for bottomBar
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
+        //set click listener for bottombar tabs
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId){
+                    case R.id.tab_archive :
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.contentContainer,new ArchiveFragment()).addToBackStack(null);
+                        break;
+                    case R.id.tab_time:
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.contentContainer,new SavedTimeFragment()).addToBackStack(null);
+                        break;
+                    case R.id.tab_new :
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.contentContainer,new NewQuoteFragment()).addToBackStack(null);
+                        break;
+                    case R.id.tab_setting :
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.contentContainer,new SettingFragment()).addToBackStack(null);
+                        break;
+                }
+                ft.commit();
+            }
+
+        });
+
+
 
 
     }
---
+++ app/src/main/java/me/sadraa/detoxiom/SavedTimeFragment.java
@@ -0,0 +1,29 @@
+package me.sadraa.detoxiom;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SavedTimeFragment extends Fragment {
+
+
+    public SavedTimeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_time, container, false);
+    }
+
+}
