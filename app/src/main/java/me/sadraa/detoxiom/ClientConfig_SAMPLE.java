39e0d7a2e0e0d788ed6a5108cd3e24718882c8d0
01sadra <01sadra@gmail.com>
2017-10-13 12:38:46 +0330

    🎨 Add client config
    client config sample added

---
+++ app/src/main/java/me/sadraa/detoxiom/ClientConfig_SAMPLE.java
@@ -0,0 +1,13 @@
+package me.sadraa.detoxiom;
+
+/**
+ * Created by sadra on 10/13/17.
+ */
+
+public class ClientConfig_SAMPLE {
+    //For getting an api token from ghesarha.ir first signup in site and then send an email to
+    //yahya : yahya.arbabi[at]gmail.com
+    public final static String api_token =" ";
+    public final static String BASE_URL ="http://ghesarha.ir/api_v1/";
+
+}
