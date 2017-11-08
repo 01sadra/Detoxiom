28b22426886c2c9af80eddc0f632eba043f3868d
01sadra <01sadra@gmail.com>
2017-11-08 10:49:20 +0330

    🚧 Animation Added to new quote fragment
    Animation Added and binding getting quotes to chance and random

--- app/src/main/java/me/sadraa/detoxiom/NewQuoteFragment.java
+++ app/src/main/java/me/sadraa/detoxiom/NewQuoteFragment.java
@@ -1,7 +1,12 @@
 package me.sadraa.detoxiom;
 
 
+import android.animation.Animator;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
@@ -12,7 +17,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 import java.io.IOException;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,12 +31,14 @@ import retrofit2.Response;
  * A simple {@link Fragment} subclass.
  */
 public class NewQuoteFragment extends Fragment {
-
     Button mButton, mButtonSave, mButtonIgonre;
     TextView quoteTV;
     TextView authorTV;
     BottomSheetBehavior mBottomSheetBehavior;
     QuoteDbModel quoteDbModel;
+    Vibrator vibrator;
+    LottieAnimationView lAnimation;
+    boolean RandomChance ;
     public NewQuoteFragment() {
         // Required empty public constructor
     }
@@ -42,52 +52,90 @@ public class NewQuoteFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     //To do : rewrite this shit with butterkinfe
         authorTV = (TextView) getView().findViewById(R.id.authorText);
-        mButton = (Button) getView().findViewById(R.id.fetchButton);
         mButtonSave = (Button) getView().findViewById(R.id.saveQuote);
         mButtonIgonre= (Button) getView().findViewById(R.id.ignoreQuote);
         quoteTV = (TextView) getView().findViewById(R.id.quoteText);
         View bottomSheet = getView().findViewById(R.id.bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        lAnimation = (LottieAnimationView) getView().findViewById(R.id.animation_refresh);
+         vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        lAnimation.setAnimation("refresh.json");
+        lAnimation.setProgress(1);
+        lAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                RandomChance = makeChance();
+                if(RandomChance){
+                    view.setBackgroundColor(getResources().getColor(R.color.primary_dark));
+                    vibrator.vibrate(500);
+                    //Creating object from provider class to get retrofit service
+                    String api_token = ClientConfig.api_token;
+                    QuoteProvider QProvider = new QuoteProvider();
+                    QuoteClient QService = QProvider.getmQService();
+            /* Call method and run it asynchronously :) */
+                    Call<QuoteModel> call = QService.getQuote(api_token);
+                    call.enqueue(new Callback<QuoteModel>() {
+                        @Override
+                        public void onResponse(Call<QuoteModel> call, Response<QuoteModel> response) {
+                            if(response.isSuccessful()){
+                                quoteTV.setText(response.body().getResult().getQuote());
+                                authorTV.setText(response.body().getResult().getAuthor());
+                                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                            }else{
+
+                                try {
+                                    Toast.makeText(getContext(),response.errorBody().string(),Toast.LENGTH_SHORT).show();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                        @Override
+                        public void onFailure(Call<QuoteModel> call, Throwable t) {
+                            Toast.makeText(getContext(),"مشکل در برقراری اتصال به اینترنت!",Toast.LENGTH_SHORT).show();
+                            t.printStackTrace();
+                        }
+                    });
+                }else{
+                  view.setBackgroundColor(getResources().getColor(R.color.about_youtube_color));
+                  Toast.makeText(getContext(),"یه بار دیگه امتحان کن",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                view.setBackgroundColor(getResources().getColor(R.color.icons));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        lAnimation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             //Creating object from provider class to get retrofit service
-                String api_token = ClientConfig.api_token;
-                QuoteProvider QProvider = new QuoteProvider();
-                QuoteClient QService = QProvider.getmQService();
-            /* Call method and run it asyncornisly :) */
-                Call<QuoteModel> call = QService.getQuote(api_token);
-                call.enqueue(new Callback<QuoteModel>() {
-
-                    @Override
-                    public void onResponse(Call<QuoteModel> call, Response<QuoteModel> response) {
-                         if(response.isSuccessful()){
-                             quoteTV.setText(response.body().getResult().getQuote());
-                             authorTV.setText(response.body().getResult().getAuthor());
-                             mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                         }else{
-
-                             try {
-                                 Toast.makeText(getContext(),response.errorBody().string(),Toast.LENGTH_SHORT).show();
-                             } catch (IOException e) {
-                                 e.printStackTrace();
-                             }
-                         }
-
+                if(isInternetConnected()){
+                    if (!lAnimation.isAnimating() && mBottomSheetBehavior.getState()==BottomSheetBehavior.STATE_COLLAPSED){
+                        lAnimation.playAnimation();
                     }
+                }else {
+                    Toast.makeText(getContext(),"به اینترنت متصل نیستید :(",Toast.LENGTH_SHORT).show();
 
-                    @Override
-                    public void onFailure(Call<QuoteModel> call, Throwable t) {
-                        Toast.makeText(getContext(),"مشکل در برقراری اتصال به اینترنت!",Toast.LENGTH_SHORT).show();
-                        t.printStackTrace();
+                }
 
-                    }
-                });
             }
         });
 
@@ -129,4 +177,17 @@ public class NewQuoteFragment extends Fragment {
         };
         new Thread(runnable).start();
     }
+
+    public boolean makeChance(){
+        Random r = new Random();
+        int mRandomNumber = r.nextInt(100) + 1;
+        return mRandomNumber%3==0;
+    }
+     public boolean isInternetConnected(){
+         ConnectivityManager connectivityManager
+                 = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+     }
+
 }
