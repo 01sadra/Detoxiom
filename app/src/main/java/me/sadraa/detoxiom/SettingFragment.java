24531f18eb20a1bb9aae291b66627f833662f701
01sadra <01sadra@gmail.com>
2017-11-16 19:52:15 +0330

    🎨 Removing the shitty setting fragment toturial and add a gif view
    A picture worth 1000 of words :)

--- app/src/main/java/me/sadraa/detoxiom/SettingFragment.java
+++ app/src/main/java/me/sadraa/detoxiom/SettingFragment.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,9 +22,7 @@ import butterknife.Unbinder;
 public class SettingFragment extends Fragment {
     boolean isImageFitToScreen;
     private Unbinder unbinder;
-    @BindView(R.id.iv_setting_1)ImageView iv1;
-    @BindView(R.id.iv_setting_2)ImageView iv2;
-    @BindView(R.id.iv_setting_3)ImageView iv3;
+
     @BindView(R.id.about_call) Button callAbout;
     @BindView(R.id.intro_call) Button callIntro;
 
@@ -55,53 +50,7 @@ public class SettingFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
-    //Adding a feature that when you click on a imageview the image view become full screen.
-    @OnClick(R.id.iv_setting_1)
-    public void makeIV1fullScreen(){
-        if(isImageFitToScreen) {
-            isImageFitToScreen=false;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300,210);
-            layoutParams.gravity= Gravity.CENTER;
-            iv1.setLayoutParams(layoutParams);
-            iv1.setAdjustViewBounds(true);
 
-        }else{
-            isImageFitToScreen=true;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500,400);
-            iv1.setLayoutParams(layoutParams);
-            iv1.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        }
-    }
-    @OnClick(R.id.iv_setting_2)
-    public void makeIV2fullScreen(){
-        if(isImageFitToScreen) {
-            isImageFitToScreen=false;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300,210);
-            layoutParams.gravity= Gravity.CENTER;
-            iv2.setLayoutParams(layoutParams);
-            iv2.setAdjustViewBounds(true);
-        }else{
-            isImageFitToScreen=true;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500,400);
-            iv2.setLayoutParams(layoutParams);
-            iv2.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        }
-    }
-    @OnClick(R.id.iv_setting_3)
-    public void makeIV3fullScreen(){
-        if(isImageFitToScreen) {
-            isImageFitToScreen=false;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300,210);
-            layoutParams.gravity= Gravity.CENTER;
-            iv3.setLayoutParams(layoutParams);
-            iv3.setAdjustViewBounds(true);
-        }else{
-            isImageFitToScreen=true;
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500,400);
-            iv3.setLayoutParams(layoutParams);
-            iv3.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        }
-    }
     @OnClick(R.id.about_call)
     public void callAboutActivity(){
         Intent intent = new Intent(getActivity(), AboutActivity.class);
