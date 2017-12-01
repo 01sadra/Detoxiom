6cf9df97d9ecbbc75084311b767bf92ea9550f2e
01sadra <01sadra@gmail.com>
2017-12-02 00:21:42 +0330

    🎨 Adding scope for teaching fragment component and dependencies

--- app/src/main/java/me/sadraa/detoxiom/features/teaching/TeachingPresenterComponent.java
+++ app/src/main/java/me/sadraa/detoxiom/features/teaching/TeachingPresenterComponent.java
@@ -1,10 +1,12 @@
 package me.sadraa.detoxiom.features.teaching;
 
 import dagger.Component;
+import me.sadraa.detoxiom.di.scopes.TeachingFragmentComponentScope;
 
 /**
  * Created by sadra on 11/29/17.
  */
+@TeachingFragmentComponentScope
 @Component(modules = TeachingPresenterModule.class)
 public interface TeachingPresenterComponent {
 
--- app/src/main/java/me/sadraa/detoxiom/features/teaching/TeachingPresenterModule.java
+++ app/src/main/java/me/sadraa/detoxiom/features/teaching/TeachingPresenterModule.java
@@ -2,13 +2,14 @@ package me.sadraa.detoxiom.features.teaching;
 
 import dagger.Module;
 import dagger.Provides;
+import me.sadraa.detoxiom.di.scopes.TeachingFragmentComponentScope;
 
 /**
  * Created by sadra on 11/29/17.
  */
 @Module
 class TeachingPresenterModule {
-
+    @TeachingFragmentComponentScope
     @Provides
     TeachingContract.Presenter teachingPresenter(TeachingPresenterImpl presenter){
         return presenter;
