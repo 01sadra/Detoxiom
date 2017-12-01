cbe7701fd907e8e8845578aa55b5aef0efa20e6d
01sadra <01sadra@gmail.com>
2017-12-02 00:20:28 +0330

    🎨 Adding scope for Saved time fragment component and dependencies

--- app/src/main/java/me/sadraa/detoxiom/features/time_save/SavedTimeComponent.java
+++ app/src/main/java/me/sadraa/detoxiom/features/time_save/SavedTimeComponent.java
@@ -1,10 +1,12 @@
 package me.sadraa.detoxiom.features.time_save;
 
 import dagger.Component;
+import me.sadraa.detoxiom.di.scopes.SavedTimeFragmentComponentScope;
 
 /**
  * Created by sadra on 11/29/17.
  */
+@SavedTimeFragmentComponentScope
 @Component(modules = SavedTimeModule.class)
 public interface SavedTimeComponent {
     void inject(SavedTimeFragment savedTimeFragment);
--- app/src/main/java/me/sadraa/detoxiom/features/time_save/SavedTimeModule.java
+++ app/src/main/java/me/sadraa/detoxiom/features/time_save/SavedTimeModule.java
@@ -2,16 +2,19 @@ package me.sadraa.detoxiom.features.time_save;
 
 import dagger.Module;
 import dagger.Provides;
+import me.sadraa.detoxiom.di.scopes.SavedTimeFragmentComponentScope;
 
 /**
  * Created by sadra on 11/29/17.
  */
 @Module
 public class SavedTimeModule {
+    @SavedTimeFragmentComponentScope
     @Provides
     SavedTimeContract.Presenter presenter(SavedTimePresenterImpl presenter){
         return presenter;
     }
+    @SavedTimeFragmentComponentScope
     @Provides
     SavedTimeContract.Interactor timeSavedInteractor(SavedTimeInteractorImpl timeSavedInteractor){
         return timeSavedInteractor;
