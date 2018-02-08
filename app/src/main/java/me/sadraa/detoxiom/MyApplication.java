59196bfccace05ff5c2cc728558aa0c947d0c9f1
01sadra <01sadra@gmail.com>
2018-02-08 07:28:49 +0330

    🔥 Remove unnessary comment in application class

--- app/src/main/java/me/sadraa/detoxiom/MyApplication.java
+++ app/src/main/java/me/sadraa/detoxiom/MyApplication.java
@@ -5,9 +5,6 @@ import android.app.Application;
 import me.sadraa.detoxiom.di.SharedPrefrencesModule;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
-/**
- * Created by sadra on 10/28/17.
- */
 
 public class MyApplication extends Application {
     private static MyAppComponent appComponent;
